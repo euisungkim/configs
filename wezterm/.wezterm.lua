b3a5f3a3e1b216a845c1152b5a9d88a752a96957
EuiSung Kim <danielkim.learns@gmail.com>
2026-04-29 14:22:44 -0400

    update wezterm font config and add opus default to zsh template

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,11 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+config.font = wezterm.font('Comic Shanns', { weight = 'Regular' })
+config.font_size = 16.0
+config.freetype_load_target = 'Light'
+config.freetype_render_target = 'HorizontalLcd'
+
 config.enable_tab_bar = false
 
 config.keys = {
